4d731e267634cfdecaefea30cf4bf75e3cf15452
MounikaJangam <mounikaj404@gmail.com>
2023-11-13 10:28:21 +0530

    shop page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Products from './Pages/Products';
+import Cart from './Pages/Cart';
+import LoginSingup from './Pages/LoginSignup';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/product' element={<Products/>}>
+          <Route path=':productId' element={<Products/>} />
+          </Route>
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/login' element={<LoginSingup/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
